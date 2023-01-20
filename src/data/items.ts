2d90ff219f6ef1aed8336f154caf914c11f2643c
ArthurGoncalves1 <arth09th@gmail.com>
2023-01-19 23:56:39 -0300

    estrutura de dados dos gastos

---
+++ src/data/items.ts
@@ -0,0 +1,22 @@
+import { Item } from '../types/Items'
+
+export const items: Item[] = [
+    {
+        date: new Date(2023, 0, 10),
+        category: "food",
+        title: "McDonalds",
+        value: 25.8
+    },
+    {
+        date: new Date(2023, 0, 13),
+        category: "rent",
+        title: "aluguel",
+        value: 2000.00
+    },
+    {
+        date: new Date(2023, 0, 14),
+        category: "food",
+        title: "market",
+        value: 140.23
+    }
+]
